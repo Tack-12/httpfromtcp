c4e15d05a090799a3680b03c3c0e01a1d0413496
Tack-12 <pranayauni1@gmail.com>
2026-06-04 16:50:52 -0500

    Created the parsing until last byte , need to add some comments & clarifications

--- internal/request/request.go
+++ internal/request/request.go
@@ -7,14 +7,46 @@ import (
 	"strings"
 )
 
+type States int
+
+const (
+	Initialized States = iota
+	Done
+)
+
 type Request struct {
 	RequestLine RequestLine
+	CurrState   States
 }
 
 type RequestLine struct {
 	HttpVersion   string
 	RequestTarget string
 	Method        string
+}
+
+func (r *Request) Parse(data []byte) (int, error) {
+
+	switch r.CurrState {
+	case Initialized:
+		n, err := parseRequestLine(string(data), r)
+
+		if err != nil {
+			return -1, fmt.Errorf("Error Occured : %s", err)
+		}
+
+		if n == 0 {
+			return 0, nil
+		}
+
+		r.CurrState = Done
+		return n, nil
+	case Done:
+		return -1, errors.New("Error trying to read data from done State")
+
+	default:
+		return -1, errors.New("Error: Unkown State")
+	}
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
@@ -25,24 +57,30 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		return nil, err
 	}
 
-	rl, err := parseRequestLine(string(req))
+	r := &Request{CurrState: Initialized}
+
+	_, err = parseRequestLine(string(req), r)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return rl, nil
+	return r, nil
 }
 
-func parseRequestLine(line string) (*Request, error) {
+func parseRequestLine(line string, req *Request) (int, error) {
 
 	s := strings.Split(line, "\r\n")
+
+	if len(s) == 1 {
+		return 0, nil
+	}
 
 	rline := strings.Split(s[0], " ")
 
 	if len(rline) != 3 {
 		err := fmt.Errorf("the data contains less value")
-		return nil, err
+		return -1, err
 	}
 
 	method := rline[0]
@@ -52,15 +90,15 @@ func parseRequestLine(line string) (*Request, error) {
 
 	if method != strings.ToUpper(method) {
 		err := errors.New("Not Capitalized String")
-		return nil, err
+		return -1, err
 	}
 
 	if httpv != "1.1" {
 		err := errors.New("HTTP Version not 1.1")
-		return nil, err
+		return -1, err
 	}
 
-	req := &Request{RequestLine{Method: method, RequestTarget: reqt, HttpVersion: httpv}}
+	req.RequestLine = RequestLine{Method: method, RequestTarget: reqt, HttpVersion: httpv}
 
-	return req, nil
+	return len(s[0]), nil
 }
